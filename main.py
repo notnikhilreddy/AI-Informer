7b58ed6580d9c460235c8b30d716edd41e2958ff
Nikhil Reddy <nikhilreddy.maligireddy1@gmail.com>
2024-07-07 22:47:42 -0400

    v2

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ from autogen import UserProxyAgent, AssistantAgent
 from twikit import Client
 import os
 
-# Load environment variables from .env file
-load_dotenv()
+keyword = "Artificial Intelligence"
+topic_count = 20
+article_count = 10
+max_period_hours = 3
 
-# Access the environment variables
+# Load environment variables
+load_dotenv()
 GROQ_MODEL_NAME = os.getenv("GROQ_MODEL_NAME")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GROQ_API_BASE = os.getenv("GROQ_API_BASE")
-USERNAME = os.getenv("XUSERNAME")
-EMAIL = os.getenv("XEMAIL")
-PASSWORD = os.getenv("XPASSWORD")
-AUTOGEN_USE_DOCKER = os.getenv("AUTOGEN_USE_DOCKER")
+RELEASE = os.getenv("RELEASE")
 
-keyword = "AI"
-topic_count = 20
+if(RELEASE == "PROD"):
+    USERNAME = os.getenv("XUSERNAME")
+    EMAIL = os.getenv("XEMAIL")
+    PASSWORD = os.getenv("XPASSWORD")
+else:
+    USERNAME = os.getenv("XUSERNAME_TEST")
+    EMAIL = os.getenv("XEMAIL_TEST")
+    PASSWORD = os.getenv("XPASSWORD_TEST")
+
+
 
 # Config dictionary
 llm_config = {
@@ -42,17 +50,23 @@ if 'x_client' not in globals():
 
 import random
 from newspaper import Article
+import pandas as pd
+import os
 
 def select_random_article(news_list):
+    if not os.path.isfile('urls.csv'):
+        df_urls = pd.DataFrame(columns=['urls', 'status'])  # Define the variable with a default value
+        df_urls.to_csv('urls.csv')
+    else:
+        df_urls = pd.read_csv('urls.csv', index_col='Unnamed: 0')
+    news, article = None, None
+    
     while True:
         #remove any news from the news_list if it is already in the csv file
-        with open('url.csv', 'r') as f:
-            urls = f.readlines()
-            urls = [url.strip() for url in urls]
-            news_list = [news for news in news_list if news['url'] not in urls]
+        news_list = [news for news in news_list if news['url'] not in df_urls['urls'].values]
         if not news_list:
             print("No more news to select")
-            return None, None
+            break
         
         news = random.choice(news_list)
         try:
@@ -61,21 +75,54 @@ def select_random_article(news_list):
             # If the article is valid, return it
             # Otherwise, continue to the next iteration of the loop
             article = Article(news['url'])
-            # save the url in a csv file
-            with open('url.csv', 'a') as f:
-                f.write(news['url'] + '\n')
             article.download()
             article.parse()
-            if article.text:
-                return news, article
+
+            if article.text and len(article.text.strip().split('\n')) > 1:
+                # Append the URL and status to the DataFrame
+                df_urls = pd.concat([pd.DataFrame([[news['url'], 'success']], columns=df_urls.columns), df_urls], ignore_index=True)
+                df_urls.to_csv('urls.csv')
+                break
+            else:
+                df_urls = pd.concat([pd.DataFrame([[news['url'], 'empty']], columns=df_urls.columns), df_urls], ignore_index=True)
+                df_urls.to_csv('urls.csv')
+                continue
         except Exception as e:
-            # Handle the error, if any
-            # You can print the error message or log it for debugging purposes
+            # Append the URL and status to the DataFrame
+            df_urls = pd.concat([pd.DataFrame([[news['url'], 'error']], columns=df_urls.columns), df_urls], ignore_index=True)
+            df_urls.to_csv('urls.csv')
             print(f"Error selecting article: {str(e)}")
+            continue
+    return news, article
 
 
 from typing import Annotated
 from gnews import GNews
+
+#delete .cache/topics.csv to reset the topics
+if os.path.isfile('.cache/topics.csv'):
+    os.remove('.cache/topics.csv')
+
+def topic_selection_tool(topics_list: Annotated[list, "The list of topics"] = None) -> str:
+    #set df_topics topic column from the topics_list and set status to pending if it's not already in the df_topics
+    if os.path.isfile('.cache/topics.csv'):
+        df_topics = pd.read_csv('.cache/topics.csv', index_col='Unnamed: 0')
+    else:
+        df_topics = pd.DataFrame(columns=['topic', 'status'])  # Define the variable with a default value
+    if topics_list:
+        for topic in topics_list:
+            if topic not in df_topics['topic'].values:
+                df_topics = pd.concat([df_topics, pd.DataFrame([[topic, 'pending']], columns=df_topics.columns)], ignore_index=True)
+    
+    topics_list = df_topics[df_topics['status'] == 'pending']['topic'].values.tolist()
+    if(len(topics_list) == 0):
+        return "No more topics to select"
+    
+    topic_selected = random.choice(topics_list)
+    df_topics.loc[df_topics['topic'] == topic_selected, 'status'] = 'selected'
+    
+    df_topics.to_csv('.cache/topics.csv')
+    return topic_selected
 
 def get_news_article_tool(topic: Annotated[str, "The topic to collect news on"], count: Annotated[int, "The number of news articles to collect from the internet"]) -> str:
     google_news = GNews()
@@ -89,59 +136,55 @@ def get_news_article_tool(topic: Annotated[str, "The topic to collect news on"],
         news, article = select_random_article(news_list)
 
         if news and article:
-            result = f'''TITLE: {news['title']}\nCONTENT: {article}\nSOURCE: "{news['url']}"'''
+            result = f'''TITLE: {news['title']}\nCONTENT: {article.text.replace('\n\n', '\n')}\nSOURCE: "{news['url']}"'''
             return result
+        if period_hours >= max_period_hours:
+            topic = topic_selection_tool(None)
         period_hours += 1  # Increase the period by 1 hour and try again
-
-# def get_news_articles_tool(topic: Annotated[str, "The topic to collect news on"], count: Annotated[int, "The number of news articles to collect from the internet"]) -> str:
-#     api_key = "bca2837056064cf9b56163348105b235"
-#     url = f"https://newsapi.org/v2/everything?q={topic}&pageSize={count}&language={'en'}&apiKey={api_key}"
-#     response = requests.get(url)
     
-#     if response.status_code == 200:
-#         articles = response.json().get('articles', [])
-#         return "\n\n".join([f"Title: {article['title']}\nDescription: {article['description']}\nSource: {article['url']}" for article in articles])
-#     else:
-#         return "Failed to fetch news articles"
-    
-def get_trending_tweets_tool(topic: Annotated[str, "The topic to retrieve tweets on"], count: Annotated[int, "The number of top tweets to collect"]) -> str:
-    tweets = []
-    tweets_batch = x_client.search_tweet(query=topic, product='Top', count=count)
+# def get_trending_tweets_tool(topic: Annotated[str, "The topic to retrieve tweets on"], count: Annotated[int, "The number of top tweets to collect"]) -> str:
+#     tweets = []
+#     tweets_batch = x_client.search_tweet(query=topic, product='Top', count=count)
 
-    while len(tweets) < count:
-        for tweet in tweets_batch:
-            if tweet.lang == 'en' and 't.co/' in tweet.full_text:
-                tweets.append(tweet.full_text)
-                if len(tweets) >= count:
-                    break
-        else:
-            tweets_batch = tweets_batch.next()
+#     while len(tweets) < count:
+#         for tweet in tweets_batch:
+#             if tweet.lang == 'en' and 't.co/' in tweet.full_text:
+#                 tweets.append(tweet.full_text)
+#                 if len(tweets) >= count:
+#                     break
+#         else:
+#             tweets_batch = tweets_batch.next()
 
-    return "\n\n".join(f'Tweet {i+1}: "{tweet}"' for i, tweet in enumerate(tweets))
+#     return "\n\n".join(f'Tweet {i+1}: "{tweet}"' for i, tweet in enumerate(tweets))
 
-def write_tweet_tool(tweet: Annotated[str, "The tweet to post"]) -> str:
+def write_tweet_tool(tweet: Annotated[str, "The tweet to post"], source: Annotated[str, "The source URL of the news"]) -> str:
     try:
-
-        # x_client.create_tweet(
-        #     text=tweet,
-        # )
+        # if tweet doesn't contain a URL, append the source URL to the tweet
+        if 'news.google.com' not in tweet:
+            tweet += f"\n{source}"
+        
+        if(RELEASE != "DEV"):
+            x_client.create_tweet(
+                text=tweet,
+            )
         
         return f'Tweet posted: "{tweet}"'
     except Exception as e:
         error_message = f"Failed to post tweet: {str(e)}"
         return error_message
 
+
 topic_selector_agent = AssistantAgent(
     "topic_selector_agent",
     llm_config=llm_config,
-    system_message=f"You are good at generating a list of topics closely related to a given input keyword. Use the provided tool to pick one random topic from the results.",
+    system_message=f"You are good at writing a list of closely related topics to the given topic(including the given topic) and then choose any one out of them. Use the provided tools for both topic selection and to collect new articles.",
     max_consecutive_auto_reply=1
 )
 
 news_collector_agent = AssistantAgent(
     "news_collector_agent",
     llm_config=llm_config,
-    system_message="You are good at collecting recent news articles about a given topic on the internet and returning one news from the list. Use the provided tool.",
+    system_message=f"You are good at collecting recent news articles about a given topic on the internet. Use the provided tool to collect news about the chosen topic.",
     max_consecutive_auto_reply=1
 )
 
@@ -152,24 +195,17 @@ news_collector_agent = AssistantAgent(
 #     max_consecutive_auto_reply=1
 # )
 
-tweets_retriever_agent = AssistantAgent(
-    "tweets_retriever_agent",
-    llm_config=llm_config,
-    system_message="You are good at retrieving recent tweets about a given topic on twitter. Always include source the URL link. Use the provided tool.",
-    max_consecutive_auto_reply=1
-)
-
-# tweets_summarizer_agent = AssistantAgent(
-#     "tweets_summarizer_agent",
+# tweets_retriever_agent = AssistantAgent(
+#     "tweets_retriever_agent",
 #     llm_config=llm_config,
-#     system_message="You are good at summarizing a list of tweets into one paragraph. You can pick a subset of tweets that are related to each other and omit others. DO NOT add any extra information.",
+#     system_message="You are good at retrieving recent tweets about a given topic on twitter. Always include source the URL link. Use the provided tool.",
 #     max_consecutive_auto_reply=1
 # )
 
 tweet_writer_agent = AssistantAgent(
     "tweet_writer_agent",
     llm_config=llm_config,
-    system_message="You are good posting twitter posts about the picked topic. You will always link to the source for all tweets. You will give your opinion where relevant.",
+    system_message="You are an autonomous twitter bot that's designed to post the latest news for everyone. You are good at posting twitter posts on the given news. Use the provided tool to post a tweet.",
     max_consecutive_auto_reply=1
 )
 
@@ -184,62 +220,56 @@ user_proxy_agent = UserProxyAgent(
 
 
 # # Register the tool signature with the assistant agent.
-# topic_selector_agent.register_for_llm(name="topic_selector_tool", description="Generate a list of topics related to the input topic and return a random topic.")(topic_selector_tool)
+topic_selector_agent.register_for_llm(name="topic_selection_tool", description="Generate a list of topics related to the input topic and return a random topic.")(topic_selection_tool)
 news_collector_agent.register_for_llm(name="get_news_article_tool", description="Collect news articles about a topic on the internet.")(get_news_article_tool)
-tweets_retriever_agent.register_for_llm(name="get_trending_tweets_tool", description="Collect top trending tweets about a topic on twitter.")(get_trending_tweets_tool)
+# tweets_retriever_agent.register_for_llm(name="get_trending_tweets_tool", description="Collect top trending tweets about a topic on twitter.")(get_trending_tweets_tool)
 tweet_writer_agent.register_for_llm(name="write_tweet_tool", description="Write a twitter post.")(write_tweet_tool)
 
 # Register the tool function with the user proxy agent.
-# user_proxy_agent.register_for_execution(name="topic_selector_tool")(topic_selector_tool)
+user_proxy_agent.register_for_execution(name="topic_selection_tool")(topic_selection_tool)
 user_proxy_agent.register_for_execution(name="get_news_article_tool")(get_news_article_tool)
-user_proxy_agent.register_for_execution(name="get_trending_tweets_tool")(get_trending_tweets_tool)
+# user_proxy_agent.register_for_execution(name="get_trending_tweets_tool")(get_trending_tweets_tool)
 user_proxy_agent.register_for_execution(name="write_tweet_tool")(write_tweet_tool)
 
-article_count = 5
-topic_selected = ''
 
-user_proxy_agent.initiate_chats([
-        {
-            "recipient": topic_selector_agent,
-            "message": f"Generate a list of {topic_count} topics related to the topic '{keyword}' and return a random topic from the list.",
-            "clear_history": True,
-            "silent": False,
-            "summary_method": "last_msg"
-        },
-        {
-            "recipient": news_collector_agent,
-            "message": f"Collect {article_count} news articles about the topic '{topic_selected}' from the internet and return a random topic from the list",
-            "clear_history": True,
-            "silent": False,
-            "summary_method": "last_msg"
-        },
-        # {
-        #     "recipient": news_picker_agent,
-        #     "message": "Pick the most interesting news article:",
-        #     "clear_history": True,
-        #     "silent": False,
-        #     "summary_method": "reflection_with_llm",
-        # },
-        # {
-        #     "recipient": tweets_retriever_agent,
-        #     "message": f"Retrieve the top {count} trending tweets about the topic '{topic_selected}' from twitter:",
-        #     "clear_history": True,
-        #     "silent": False,
-        #     "summary_method": "last_msg",
-        # },
-        # {
-        #     "recipient": tweets_summarizer_agent,
-        #     "message": "Summarize the list of tweets:",
-        #     "clear_history": True,
-        #     "silent": False,
-        #     "summary_method": "reflection_with_llm",
-        # },
-        {
-            "recipient": tweet_writer_agent,
-            "message": "Write and post a twitter post about the given news article and link to the source.",
-            "clear_history": True,
-            "silent": False,
-            "summary_method": "last_msg"
-        }
-    ]
-)
+try:
+    user_proxy_agent.initiate_chats([
+            {
+                "recipient": topic_selector_agent,
+                "message": f"Generate a list of {topic_count} topics related to the topic '{keyword}' and return a random topic from the list.",
+                "clear_history": True,
+                "silent": False,
+                "summary_method": "last_msg"
+            },
+            {
+                "recipient": news_collector_agent,
+                "message": f"Collect {article_count} news articles about the given topic from the internet.",
+                "clear_history": True,
+                "silent": False,
+                "summary_method": "last_msg"
+            },
+            # {
+            #     "recipient": news_picker_agent,
+            #     "message": "Pick the most interesting news article:",
+            #     "clear_history": True,
+            #     "silent": False,
+            #     "summary_method": "reflection_with_llm",
+            # },
+            # {
+            #     "recipient": tweets_retriever_agent,
+            #     "message": f"Retrieve the top {count} trending tweets about the topic '{topic_selected}' from twitter:",
+            #     "clear_history": True,
+            #     "silent": False,
+            #     "summary_method": "last_msg",
+            # },
+            {
+                "recipient": tweet_writer_agent,
+                "message": "Write and post a twitter post about the given news article and link to the source.",
+                "clear_history": True,
+                "silent": False,
+                "summary_method": "last_msg"
+            }
+        ]
+    )
+except Exception as e:
+    print(f"Error: {str(e)}")
